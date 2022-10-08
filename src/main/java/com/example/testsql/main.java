ae9d6fba3f88fa58ece2a82e80bca1d27e0b7e4f
FallOfTheHell <gleb.epifancev@mail.ru>
2022-10-08 18:21:44 +0700

    Initial commit

---
+++ src/main/java/com/example/testsql/main.java
@@ -0,0 +1,36 @@
+package com.example.testsql;
+
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class main {
+        static ServerSocket server;
+        static Socket socket;
+    public static void main(String[] args) {
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Server start...");
+            socket = server.accept();
+            System.out.println("Клиент подключился");
+            Scanner sc = new Scanner(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            while (true){
+                String str = sc.nextLine();
+                System.out.println("Клиент написал: " + str);
+                if (str.equals("/end")){
+                    break;
+                }
+                out.writeUTF("Сервер: " + str);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
